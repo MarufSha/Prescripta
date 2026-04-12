30584221f7f62c57a1245fdd2afe3d91c4ef5b73
MarufSha <niloy9896@gmail.com>
2026-04-12 12:02:36 +0600

    Change doctor invite validator size to comply with rest of the project

--- backend/validators/doctorInviteValidators.js
+++ backend/validators/doctorInviteValidators.js
@@ -4,8 +4,8 @@ export const acceptDoctorInviteValidation = [
   body("password")
     .isString()
     .withMessage("Password must be a string")
-    .isByteLength({ min: 8, max: 72 })
-    .withMessage("Password must be between 8 and 72 bytes"),
+    .isByteLength({ min: 8, max: 128 })
+    .withMessage("Password must be between 8 and 128 bytes"),
 
   body("doctorProfile")
     .isObject()
